f351a690b65428bef8daee988b62f62f09cf4618
MunasheCraig <munashe.mhonda@omnicontact.biz>
2025-09-05 10:08:54 +0200

    initial server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'Server is running!' 
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
